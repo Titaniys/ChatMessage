10a06bcd960d8705b923a498b2234e2b75ec74b1
Чистяков Вадим Евгеньевич <vchistyakov@stream.ru>
2018-11-09 17:09:41 +0300

    Initial Commit

---
+++ ChatMessageKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatMessageKit
+//
+//  Created by Чистяков Вадим Евгеньевич on 09/11/2018.
+//  Copyright © 2018 Chist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
