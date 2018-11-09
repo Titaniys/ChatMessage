7ad4fcaae03789a7a40670bd5628627e372895d7
Чистяков Вадим Евгеньевич <vchistyakov@stream.ru>
2018-11-09 18:34:52 +0300

    add messages protocols implementation

---
+++ ChatMessageKit/Models/Message.swift
@@ -0,0 +1,36 @@
+//
+//  Message.swift
+//  ChatMessageKit
+//
+//  Created by Чистяков Вадим Евгеньевич on 09/11/2018.
+//  Copyright © 2018 Chist. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import MessageKit
+
+struct Member {
+    let name: String
+    let color: UIColor
+}
+
+struct Message {
+    let member: Member
+    let text: String
+    let messageId: String
+}
+
+extension Message: MessageType {
+    var sentDate: Date {
+        return Date()
+    }
+
+    var kind: MessageKind {
+        return .text(text)
+    }
+
+    var sender: Sender {
+        return Sender(id: member.name, displayName: member.name)
+    }
+}
