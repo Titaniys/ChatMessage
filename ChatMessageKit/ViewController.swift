8d4ba5d4b01649e7d072082083048f317a750fdd
Чистяков Вадим Евгеньевич <vchistyakov@stream.ru>
2018-11-23 19:02:07 +0300

    add simple chat

---
+++ ChatMessageKit/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  ChatMessageKit
+//
+//  Created by Чистяков Вадим Евгеньевич on 09/11/2018.
+//  Copyright © 2018 Chist. All rights reserved.
+//
+
+import UIKit
+import MessageKit
+
+class ViewController: MessagesViewController {
+
+    var messages: [Message] = []
+    var member: Member!
+    var chatService: ChatService!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        member = Member(name: .randomName, color: .random)
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        messageInputBar.delegate = self
+
+        chatService = ChatService(member: member, onRecievedMessage: { [weak self] message in
+            self?.messages.append(message)
+            self?.messagesCollectionView.reloadData()
+            self?.messagesCollectionView.scrollToBottom(animated: true)
+        })
+
+        chatService.connect()
+    }
+
+}
+
+extension ViewController: MessagesDataSource {
+    func currentSender() -> Sender {
+        return Sender(id: member.name, displayName: member.name)
+    }
+
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+
+    func messageTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        return NSAttributedString(string: message.sender.displayName, attributes: [.font: UIFont.systemFont(ofSize: 12)])
+    }
+
+    func messageTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 12
+    }
+}
+
+extension ViewController: MessagesLayoutDelegate {
+    func heightForLocation(message: MessageType, at indexPath: IndexPath, with maxWidth: CGFloat, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 0
+    }
+}
+
+extension ViewController: MessagesDisplayDelegate {
+    func configureAvatarView(_ avatarView: AvatarView, for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) {
+        let message = messages[indexPath.section]
+        let color = message.member.color
+        avatarView.backgroundColor = color
+    }
+}
+
+extension ViewController: MessageInputBarDelegate {
+    func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
+        let newMessage = Message(member: member, text: text, messageId: UUID().uuidString)
+        messages.append(newMessage)
+        inputBar.inputTextView.text = ""
+        messagesCollectionView.reloadData()
+        messagesCollectionView.scrollToBottom(animated: true)
+        chatService.sendMessage(text)
+        inputBar.inputTextView.text = ""
+    }
+}
